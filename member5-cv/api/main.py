2f8dec35655dd124cd3a87d0ade855c56f073cc8
Rajeshwari05-V <rajeshwari201005@gmail.com>
2026-08-01 13:10:58 +0530

    Fix CORS

--- member5-cv/api/main.py
+++ member5-cv/api/main.py
@@ -30,7 +30,9 @@ app = FastAPI(title="Fire Safety CV API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
